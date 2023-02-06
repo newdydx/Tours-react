1f25571fc94c8e9aa25dac63cbe4330a323af0ad
heritage_dx <109880414+newdydx@users.noreply.github.com>
2023-02-06 10:34:19 +0100

    space

--- src/app.js
+++ src/app.js
@@ -66,7 +66,7 @@ const App = () => {
                 <p>
                   {readMore ? info : `${info.substring(0, 200)}...`}
                   <button onClick={() => setReadMore(!readMore)}>
-                    {readMore ? "show less" : "  read more"}
+                    {readMore ? "show less" : "read more"}
                   </button>
                 </p>
                 <button
